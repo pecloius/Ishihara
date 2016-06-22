fb45735c0dfc53ddaaa069440773746808918759
chanPecko <peckoz@hotmail.com>
2016-06-22 16:41:22 +0700

    Have Problem about Variable

--- Ishihara/ViewController.swift
+++ Ishihara/ViewController.swift
@@ -17,8 +17,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerTextField: UITextField!
     
     // Explicit
+    
     var strAnswer:String = ""
     var intIndex:Int = 0
+    var arrayImage = ["ishihara_01.png","ishihara_02.png","ishihara_03.png",
+                      "ishihara_04.png","ishihara_05.png","ishihara_06.png",
+                      "ishihara_07.png","ishihara_08.png","ishihara_09.png",
+                      "ishihara_10.png"]
+    var myAnswer = ["3","5","6","12","29","45","74","42","0","0"]
+    var score:Int =  0
     
     
     
@@ -41,10 +48,26 @@ class ViewController: UIViewController {
         strAnswer = String(answerTextField.text)
         print("strAnswer ==> \(strAnswer)")
         
-        intIndex += 1
+        if (intIndex < 9) {
+            intIndex += 1
+            
+        } else {
+            intIndex = 0
+        }
+    
         print("intIndex ==> \(intIndex)")
-        
-        
+    
+        ishiharaImageView.image = UIImage(named: arrayImage[intIndex])
+    
+        if (strAnswer == myAnswer[intIndex]){
+            score += 1
+            print("Score ==> \(score)")
+            
+        }
+    
+    
+    
+    
     } // answerButton
     
     
